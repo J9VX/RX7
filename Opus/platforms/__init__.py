650c9eba9af726555fb85bed8a0dc55784848526
J9VX <dhruvraj500@gmail.com>
2025-05-10 06:56:37 +0530

    Update __init__.py

--- Opus/platforms/__init__.py
+++ Opus/platforms/__init__.py
@@ -4,10 +4,10 @@ from .Resso import RessoAPI
 from .Soundcloud import SoundAPI
 from .Spotify import SpotifyAPI
 from .Telegram import TeleAPI
-from .JioSavan import Saavn
+"from .JioSavan import Saavn"
 from .Youtube import YouTubeAPI
 
 
-class PlaTForms:
+"""class PlaTForms:
     def __init__(self):
-        self.saavn = Saavn()
+        self.saavn = Saavn()"""
